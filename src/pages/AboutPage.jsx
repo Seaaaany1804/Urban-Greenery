3c8cecda090295dc209ea769e7ad85bb7c073d6c
Seaaaany1804 <nejihyuga979@gmail.com>
2024-09-26 01:05:24 +0800

    FInished AboutPage Responsive Design

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -9,15 +9,15 @@ import { HiMiniScissors } from "react-icons/hi2";
 
 function AboutPage() {
   return (
-    <div className='hidden h-screen bg-white'>
+    <div className='h-screen bg-white'>
       <div className='h-2/6 bg-primary'>
       <div className='flex justify-center items-center h-full'>
-        <div className='grid grid-cols-3 gap-8 place-items-center'>
-          <div className='tracking-wider text-[50px] font-dm-serif text-white'>
-            Grow Plant For <br/> A Better Life
+        <div className='grid lg:grid-cols-2 xl:grid-cols-3 grid-cols-1 gap-8 place-items-center'>
+          <div className='tracking-wider text-center lg:text-start w-[300px] xl:text-[50px] xl:w-[400px] text-[40px] lg:text-[40px] font-dm-serif text-white'>
+            Grow Plant For A Better Life
           </div>
 
-          <div className='relative'>
+          <div className='hidden xl:block relative'>
             <div className='absolute bottom-0 left-[-90px] inline-block px-8 bg-white rounded-full ring-2 ring-black'>
             <img src={growPlant1} alt="Grow Plant" className='w-[65px]' />
             </div>
@@ -28,10 +28,10 @@ function AboutPage() {
           </div>
 
           <div className='tracking-widest text-[16px] text-white font-poppins '>
-            <h1> Cultivating plants enhances well-being <br/>
+            <h1 className='hidden lg:block'> Cultivating plants enhances well-being <br/>
             and contributes to a healthier environment, <br/>
             fostering a more fulfilling life. </h1>
-            <button type="button" className='flex items-center text-[14px] font-poppins mt-8 bg-tertiary p-4 px-8 rounded-full'> Read More  
+            <button type="button" className='flex items-center text-[14px] lg:mt-8 font-poppins bg-tertiary p-4 px-8 rounded-full'> Read More  
               <FaCircleArrowRight className='ml-4 text-[20px]'/>
              </button>
           </div>
@@ -43,27 +43,27 @@ function AboutPage() {
 
       <div className='h-4/6 pt-12'>
         <div className='flex justify-center'>
-          <h1 className='text-primary font-dm-serif text-[40px]'> Step to start taking care of your plants </h1>
+          <h1 className='text-primary font-dm-serif text-[20px] lg:text-[40px]'> Step to start taking care of your plants </h1>
         </div>
         
         <div className='flex justify-center'>
-        <h1 className='text-black font-poppins tracking-wider mt-4 text-[16px]'> The right solution for the care of green and smart plants</h1>
+        <h1 className='text-black font-poppins tracking-wider mt-4 text-[12px] lg:text-[16px]'> The right solution for the care of green and smart plants</h1>
         </div>
 
         <div className='pt-8 flex justify-center h-4/6'>
-        <div className='grid grid-cols-3 place-items-center gap-52 mx-24'>
+        <div className='grid grid-rows-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 lg:gap-52 mx-24 place-items-center'>
 
           {/* First Grid */}
           <div className='flex flex-col items-center space-x-4'>
               <div className='bg-primary rounded-full p-2'>
-                <IoWaterOutline className='text-[60px] text-white'/>
+                <IoWaterOutline className='text-[38px] lg:text-[60px]  text-white'/>
               </div>   
 
               <div className='mt-4'>
-                <h1 className='text-[22px] font-bold font-poppins'> Humidity Control</h1>
+                <h1 className='text-[20px] lg:text-[22px] text-center font-bold font-poppins'> Humidity Control</h1>
               </div> 
 
-              <div className='text-center text-[16px] tracking-wider font-poppins mt-4'>
+              <div className='hidden xl:block text-center text-[16px] tracking-wider font-poppins mt-4'>
                 <h1> Effective humidity control <br/> is essential
                   for properly <br/> caring for plants, ensuring <br/> optimal
                   growth and overall health. </h1>
@@ -72,14 +72,14 @@ function AboutPage() {
 
           <div className='flex flex-col items-center space-x-4'>
               <div className='bg-primary rounded-full p-2'>
-                <TbSpray className='text-[60px] text-white'/>
+                <TbSpray className='text-[38px] lg:text-[60px] text-white'/>
               </div>   
 
               <div className='mt-4'>
-                <h1 className='text-[22px] font-bold font-poppins'> Pest Anticipation</h1>
+                <h1 className='text-[20px] lg:text-[22px] text-center font-bold font-poppins'> Pest Anticipation</h1>
               </div> 
 
-              <div className='text-center text-[16px] tracking-wider font-poppins mt-4'>
+              <div className='hidden xl:block  text-center text-[16px] tracking-wider font-poppins mt-4'>
                 <h1> Implementing proactive <br/> pest anticipation measures <br/> 
                 is issential to safeguard <br/> and nurture the well-being of plants. </h1>
               </div>
@@ -87,14 +87,14 @@ function AboutPage() {
 
           <div className='flex flex-col items-center space-x-4'>
               <div className='bg-primary rounded-full p-2'>
-                <HiMiniScissors className='text-[60px] text-white'/>
+                <HiMiniScissors className='text-[38px] lg:text-[60px] text-white'/>
               </div>   
 
               <div className='mt-4'>
-                <h1 className='text-[22px] font-bold font-poppins'> Pruning Weeds </h1>
+                <h1 className='text-[20px] lg:text-[22px] text-center font-bold font-poppins'> Pruning Weeds </h1>
               </div> 
 
-              <div className='text-center text-[16px] tracking-wider font-poppins mt-4'>
+              <div className='hidden xl:block  text-center text-[16px] tracking-wider font-poppins mt-4'>
                 <h1> Taking care of plants <br/> involves diligently
                 managing <br/> unwanted growth <br/> to ensure their well-being. </h1>
               </div>
