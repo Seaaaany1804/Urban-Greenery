bc7a62a596321c56f6bdc2fe6533242577cc6304
Seaaaany1804 <nejihyuga979@gmail.com>
2024-09-29 15:10:04 +0800

    Finished LG Responsiveness in ContactPage

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -14,22 +14,24 @@ function ContactPage() {
       <div className='relative flex justify-center space-x-4 mt-8'>
         <input type="email" name="Email" placeholder='Email' id="1" className='bg-[#385444] p-2 md:p-3 xl:p-4 xl:w-[23%] w-[60%] sm:w-[40%] text-white rounded-md font-poppins'/>
         <button type="button" className='bg-tertiary px-2 rounded-md'> <BiLogoTelegram className=' text-[24px] md:text-[32px] xl:text-[40px] text-white'/>  </button>
-        <div className='absolute bottom-[-20px] xl:bottom-[-50px] md:bottom-[-30px] w-[80%] sm:w-[60%] border-[1px] border-white'>  </div>
+        <div className='absolute bottom-[-20px] xl:bottom-[-50px] md:bottom-[-30px] w-[80%] sm:w-[60%] lg:w-[80%] border-[1px] border-white'>  </div>
       </div>
 
-      <div className='mt-8 grid-cols-1 grid xl:grid-cols-[1fr_2fr_1fr] place-items-center'>
-        <div className='flex justify-center relative w-full'> 
+      <div className='mt-8 grid-cols-1 grid lg:grid-cols-2 xl:grid-cols-3 place-items-center'>
+        {/* Information Details */}
+        <div className='flex justify-center relative w-full items-center h-full'> 
             <h1 className='text-white font-poppins text-center text-[14px]'> 123 Hello Street 
             3024, Philippines <br/> 
             +639912345678 <br/>
             example@gmail.com</h1>
-            <div className='hidden absolute right-[-100px] top-[-30px] h-[160px] border-[1px] border-white'>  </div>
-            <div className='absolute bottom-[-10px] xl:bottom-[-50px] md:bottom-[-10px] w-[80%] sm:w-[60%] border-[1px] border-white'></div>
+            <div className='hidden lg:block absolute right-[0px] top-[10px] h-[100%] border-[1px] border-white'>  </div>
+            <div className='lg:hidden absolute bottom-[-10px] xl:bottom-[-50px] md:bottom-[-10px] w-[80%] sm:w-[60%] border-[1px] border-white'></div>
         </div>
-      
+
+        {/* Title */}
         <div className='w-full items-center relative text-center flex flex-col justify-center'>
             <h1 className='text-white font-semibold font-poppins tracking-wider text-[26px] mt-4'> URBAN GREENERY </h1>
-            <h1 className='hidden text-white tracking-widest text-[16px] mt-4'> Caring for plants enhances your space with life. <br/>
+            <h1 className='hidden xl:block text-white tracking-widest text-[16px] mt-4'> Caring for plants enhances your space with life. <br/>
             Whether it's a small herb garden or thriving jungle, nurturing your plants <br/>
             rewards you with greenery and serenity.</h1>
                 <div className='flex justify-center gap-5 mt-2'>
@@ -37,22 +39,37 @@ function ContactPage() {
                     <FaInstagram className='text-white text-[30px]' />
                     <FaYoutube className='text-white text-[32px]' />
                 </div>
-            <div className='absolute bottom-[-10px] xl:bottom-[-50px] md:bottom-[-10px] w-[80%] sm:w-[60%] border-[1px] border-white'></div>    
+            <div className='lg:hidden absolute bottom-[-10px] xl:bottom-[-50px] md:bottom-[-10px] w-[80%] sm:w-[60%] border-[1px] border-white'></div>    
             <div className='hidden absolute right-[-100px] top-[0px] h-[160px] border-[1px] border-white'>  </div>
         </div>
 
-        <div className='mt-4 flex flex-col items-center justify-center'>
+        {/* Pages */}
+        <div className='mt-4 lg:mt-0 lg:hidden xl:block flex flex-col items-center justify-center'>
                 <h1 className='text-white text-[20px] text-center font-poppins tracking-wider font-semibold'> Pages </h1>
-                <div className='flex gap-4 text-white font-poppins tracking-wider mt-4'>
+                <div className='flex gap-4 text-white font-poppins xl:flex-col xl:gap-1 tracking-wider mt-4'>
+                    <a href="Home" className='hover:underline'> Home </a>
+                    <a href="Shop" className='hover:underline'> Shop </a>
+                    <a href="About" className='hover:underline'> About </a>
+                    <a href="Blog" className='hover:underline'> Blog </a>
+                    <a href="Contact" className='hover:underline'> Contact </a>
+                </div>         
+                
+        </div>  
+      </div>
+
+      <div className='hidden lg:block xl:hidden mt-4 lg:mt-0 flex flex-col items-center justify-center'>
+        <div className='relative flex justify-center'>
+          <div className='absolute top-[20px] w-[80%] border-[1px] border-white'>  </div>
+        </div>
+                <h1 className='text-white mt-8 text-[20px] text-center font-poppins tracking-wider font-semibold'> Pages </h1>
+                <div className='flex justify-center gap-4 text-white font-poppins tracking-wider mt-4'>
                     <a href="Home" className='hover:underline'> Home </a>
                     <a href="Shop" className='hover:underline'> Shop </a>
                     <a href="About" className='hover:underline'> About </a>
                     <a href="Blog" className='hover:underline'> Blog </a>
                     <a href="Contact" className='hover:underline'> Contact </a>
                 </div>
-                     
-        </div>        
-      </div>
+        </div>  
 
       
       <div className='flex justify-center relative'>
