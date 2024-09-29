671c401f0156fb8c86502624f6f62d10701b9d52
Seaaaany1804 <nejihyuga979@gmail.com>
2024-09-29 15:24:38 +0800

    Finished XL and 2xl Responsiveness in ContactPage

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -17,35 +17,35 @@ function ContactPage() {
         <div className='absolute bottom-[-20px] xl:bottom-[-50px] md:bottom-[-30px] w-[80%] sm:w-[60%] lg:w-[80%] border-[1px] border-white'>  </div>
       </div>
 
-      <div className='mt-8 grid-cols-1 grid lg:grid-cols-2 xl:grid-cols-3 place-items-center'>
+      <div className='mt-8 grid-cols-1 grid lg:grid-cols-2 xl:grid-cols-[1fr_2fr_1fr] place-items-center'>
         {/* Information Details */}
         <div className='flex justify-center relative w-full items-center h-full'> 
-            <h1 className='text-white font-poppins text-center text-[14px]'> 123 Hello Street 
+            <h1 className='text-white font-poppins text-center text-[14px] 2xl:text-[18px]'> 123 Hello Street 
             3024, Philippines <br/> 
             +639912345678 <br/>
             example@gmail.com</h1>
-            <div className='hidden lg:block absolute right-[0px] top-[10px] h-[100%] border-[1px] border-white'>  </div>
+            <div className='hidden lg:block absolute right-[0px] top-[10px] xl:top-[40px] h-[100%] xl:h-[80%] border-[1px] border-white'>  </div>
             <div className='lg:hidden absolute bottom-[-10px] xl:bottom-[-50px] md:bottom-[-10px] w-[80%] sm:w-[60%] border-[1px] border-white'></div>
         </div>
 
         {/* Title */}
         <div className='w-full items-center relative text-center flex flex-col justify-center'>
-            <h1 className='text-white font-semibold font-poppins tracking-wider text-[26px] mt-4'> URBAN GREENERY </h1>
-            <h1 className='hidden xl:block text-white tracking-widest text-[16px] mt-4'> Caring for plants enhances your space with life. <br/>
-            Whether it's a small herb garden or thriving jungle, nurturing your plants <br/>
+            <h1 className='text-white font-semibold font-poppins tracking-wider text-[20px] md:text-[26px] mt-4 xl:mt-8'> URBAN GREENERY </h1>
+            <h1 className='hidden xl:block text-white tracking-widest text-[16px] mt-4 xl:w-[70%] 2xl:w-[60%]'> Caring for plants enhances your space with life. 
+            Whether it's a small herb garden or thriving jungle, nurturing your plants 
             rewards you with greenery and serenity.</h1>
-                <div className='flex justify-center gap-5 mt-2'>
-                    <FaFacebook className='text-white text-[30px]' />
-                    <FaInstagram className='text-white text-[30px]' />
-                    <FaYoutube className='text-white text-[32px]' />
+                <div className='flex justify-center gap-5 mt-2 2xl:mt-4'>
+                    <FaFacebook className='text-white text-[30px] 2xl:text-[40px]' />
+                    <FaInstagram className='text-white text-[30px] 2xl:text-[40px]' />
+                    <FaYoutube className='text-white text-[32px] 2xl:text-[42px]' />
                 </div>
             <div className='lg:hidden absolute bottom-[-10px] xl:bottom-[-50px] md:bottom-[-10px] w-[80%] sm:w-[60%] border-[1px] border-white'></div>    
-            <div className='hidden absolute right-[-100px] top-[0px] h-[160px] border-[1px] border-white'>  </div>
+            <div className='hidden xl:block absolute right-[0px] top-[40px] h-[80%] border-[1px] border-white'>  </div>
         </div>
 
         {/* Pages */}
-        <div className='mt-4 lg:mt-0 lg:hidden xl:block flex flex-col items-center justify-center'>
-                <h1 className='text-white text-[20px] text-center font-poppins tracking-wider font-semibold'> Pages </h1>
+        <div className='mt-4 lg:mt-0 xl:mt-8 lg:hidden xl:block flex flex-col items-center justify-center'>
+                <h1 className='text-white text-[20px] text-[18px] md:text-[20px] text-center font-poppins tracking-wider font-semibold'> Pages </h1>
                 <div className='flex gap-4 text-white font-poppins xl:flex-col xl:gap-1 tracking-wider mt-4'>
                     <a href="Home" className='hover:underline'> Home </a>
                     <a href="Shop" className='hover:underline'> Shop </a>
@@ -73,7 +73,7 @@ function ContactPage() {
 
       
       <div className='flex justify-center relative'>
-        <div className='absolute mt-8 w-[80%] sm:w-[60%]  border-[1px] border-white'>  </div>
+        <div className='absolute mt-8 w-[80%] sm:w-[60%] xl:w-[80%] border-[1px] border-white'>  </div>
         <h1 className='mt-16 text-center text-white font-poppins tracking-widest text-[12px]'> © 2023 URBAN GREENERY | ALL RIGHTS RESERVED <br/>
         MADE BY SEAN DEXTER LAGMAY </h1>
       </div>
